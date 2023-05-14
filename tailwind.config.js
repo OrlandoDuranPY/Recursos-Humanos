7681b55d9bd0aa279a0fe7731e25fcf54a71d179
orlandoduranpy <orlandoduranpy@outlook.es>
2023-05-14 16:39:45 -0600

    Formularios de Login y Signin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Manrope', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 }
